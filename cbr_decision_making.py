90f134d601a341dcda1c0df89cfd5d435d45118c
Matheus Chagas <matheus.chagas@aluno.uece.br>
2023-08-28 08:26:48 -0300

    First commit with some class definitions

---
+++ cbr_decision_making.py
@@ -0,0 +1,15 @@
+class DecisionMaking:
+
+    def __init__(self):
+        pass
+
+    #TODO: CRUD benefit
+
+    #TODO: CRUD cost
+
+    #TODO: CRUD risk
+
+    #TODO: calculate CBR@R
+    def calculate_cbr():
+        pass
+
---
+++ main.py
@@ -0,0 +1,8 @@
+class Attribute:
+
+    def __init__(self, name):
+        self.value = 0
+        self.weight = 1
+        self.name = name
+
+    
