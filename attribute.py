70fb8aec1b8ca1e1c0d429e3c0271be2497e8eff
Matheus Chagas <maths.c28@gmail.com>
2023-08-28 17:06:23 -0300

    add utility preference to Attribute class

--- attribute.py
+++ attribute.py
@@ -1,10 +1,15 @@
+from utility_preference import UtilityPreference
+
 class Attribute:
 
-    def __init__(self, name, value = 0, weight = 1):
+    def __init__(self, name, value = 0, weight = 1, utility_preference = UtilityPreference()):
         self.name = name
         self.value = value
         self.weight = weight
+        self.utility_preference = utility_preference
     
     def __str__(self) -> str:
         return f"Attribute: {self.name}, Value: {self.value}, Weight: {self.weight}"
 
+    def get_utility_preference(self):
+        self.utility_preference.calculate_utility(self.value)
